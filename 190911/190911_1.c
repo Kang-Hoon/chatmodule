5d5727d3bf897c42569e35ab910c4e40557cc2f8
Kang-Hoon <48863211+Kang-Hoon@users.noreply.github.com>
2019-09-16 15:18:30 +0900

    Add files via upload

---
+++ 190911/190911_1.c
@@ -0,0 +1,18 @@
+/*		
+	Date : 11th Sep 2019
+	Author : Lee Kang-Hoon
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char a = -5;
+	unsigned char b - 251;
+	a = a-5;
+	b = b-5;
+	// printf(“%02X, %02X \n”, (unsigned char)a, b);
+	printf(“%d %d \n”, a, b);
+	printf(“%u %u \n”, (unsigned char)a, b);
+
+	return 0;
+}
